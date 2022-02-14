25a41085e3f572952ef03d8138b0345b3c0139e9
Heena Mandal <99394465+heenamandalgithub@users.noreply.github.com>
2022-02-14 14:48:10 +0530

    Add files via upload

---
+++ Ass4a.py
@@ -0,0 +1,8 @@
+#Write a program to demonstrate use of arithmetic operators(numeric as well as str object)
+x=23
+y=12
+print("x+y=",x+y)
+print("x-y=",x-y)
+print("x*y=",x*y)
+print("x/y=",x/y)
+print("x//y=",x//y)
---
+++ Ass4c.py
@@ -0,0 +1,9 @@
+#Write a program to demonstrate use of comparison operators
+x=25
+y=14
+print("x>y is",x>y)
+print("x<y is",x<y)
+print("x==y is",x==y)
+print("x!=y is",x!=y)
+print("x>=y is",x>=y)
+print("x<=y is",x<=y)
